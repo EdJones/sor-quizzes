689cb323de34d8958f5f704bcfcb2c80c35fa022
Ed <ed.jones@gmail.com>
2025-02-15 09:34:01 -0500

    Copy NewItem to views/QuizItemEditor

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ import Home from '../components/Home.vue';
 import NewItem from '../components/NewItem.vue';
 import GitHubIssues from '../components/GitHubIssues.vue';
 import QuizSetView from '../views/QuizSetView.vue';
-import QuizItemEditor from '../components/QuizItemEditor.vue';
+import QuizItemEditor from '../views/QuizItemEditor.vue';
 
 const routes = [
   {
