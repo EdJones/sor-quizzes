3c2faf1666131c41ce0c904d2774a61782fc7705
Ed <ed.jones@gmail.com>
2025-02-17 06:12:42 -0500

    Add a button to set viewer to create quiz item

--- src/views/QuizSetView.vue
+++ src/views/QuizSetView.vue
@@ -1,7 +1,14 @@
 <template>
     <div class="p-4">
-        <div class="flex justify-between items-center mb-4">
-            <h1 class="text-2xl">Quiz Set View</h1>
+        <div class="flex justify-end gap-4 mb-4">
+            <button @click="showCreateModal = true"
+                class="bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600 transition-colors duration-200 flex items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24"
+                    stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
+                </svg>
+                Create/Edit A Quiz Item
+            </button>
             <button @click="showCreateModal = true"
                 class="bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600 transition-colors duration-200 flex items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24"
